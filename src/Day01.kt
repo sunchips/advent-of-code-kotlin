81d779161e1d4f4b0763bcd871640aeb12b93af5
Sanchit M. Bhatnagar <sanchitbhatnagar@gmail.com>
2022-12-20 23:10:45 +0100

    Day01 both parts completed

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,44 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var max = Integer.MIN_VALUE
+        var tmp = 0
+        for (food in input) {
+            if (food == "") {
+                // next elf
+                if (tmp > max) {
+                    max = tmp
+                }
+                tmp = 0
+                continue
+            }
+            tmp += food.toInt()
+        }
+        if (tmp > max) {
+            max = tmp
+        }
+        return max
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val foodCounts = mutableListOf<Int>()
+        var tmp = 0
+        for (food in input) {
+            if (food == "") {
+                // next elf
+                foodCounts.add(tmp)
+                tmp = 0
+                continue
+            }
+            tmp += food.toInt()
+        }
+        foodCounts.add(tmp)
+        foodCounts.sortDescending()
+        return foodCounts[0] + foodCounts[1] + foodCounts[2]
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     part1(input).println()
